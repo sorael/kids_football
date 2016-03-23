c97e20d3fcfce3fd521bcd14bd24e31471113e65
Anatolii Soroka <sorokaanatolii@gmail.com>
2016-03-23 15:57:49 +0200

    edit_game, my_games. refactored templates, views

--- game/urls.py
+++ game/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'game.views.index', name='index'),
     url(r'^create_game/', 'game.views.create_game', name='create_game'),
+    url(r'^my_games/', 'game.views.my_games', name='my_games'),
+    url(r'^edit_game/(?P<game_id>\d+)/$', 'game.views.edit_game', name='edit_game'),
 
 )
--- game/views.py
+++ game/views.py
@@ -10,9 +10,15 @@ from game.forms import CreateGameForm
 
 
 def index(request, page=1):
+    try:
+        user = request.user
+        profile, created = Profile.objects.get_or_create(user=user)
+    except TypeError:
+        profile = None
     games = Paginator(Game.objects.all().select_related('creator__user'), 5)
     return render(request, 'game/index.html',
-                  {'games': games.page(page)})
+                  {'games': games.page(page),
+                   'profile': profile})
 
 
 @login_required
@@ -42,4 +48,48 @@ def create_game(request):
                                 content_type='application/json')
     return render(request, 'game/create_game.html',
                   {'create_game_form': CreateGameForm(),
+                   'profile': profile})
+
+
+@login_required
+def edit_game(request, game_id):
+    user = request.user
+    game = Game.objects.select_related('creator').get(id=game_id)
+    if user == game.creator.user:
+        profile = Profile.objects.get(user=user)
+        response = {}
+        if request.POST and request.is_ajax():
+            edit_game_form = CreateGameForm(request.POST, instance=game)
+            if edit_game_form.is_valid():
+                edit_game_form.save()
+                response['success'] = 'true'
+                return HttpResponse(json.dumps(response),
+                                    content_type='application/json')
+            else:
+                if edit_game_form.errors:
+                    errs = {}
+                    for error in edit_game_form.errors:
+                        e = edit_game_form.errors[error]
+                        errs[error] = unicode(e)
+                    response['success'] = 'false'
+                    response['errors'] = errs
+                return HttpResponse(json.dumps(response),
+                                    content_type='application/json')
+        return render(request, 'game/edit_game.html',
+                      {'edit_game_form': CreateGameForm(instance=game),
+                       'profile': profile,
+                       'game': game})
+    else:
+        return render(request, 'game/edit_game.html',
+                      {'access_denied': 'access_denied'})
+
+
+
+@login_required
+def my_games(request):
+    user = request.user
+    profile = Profile.objects.get(user=user)
+    games = Game.objects.all().select_related('creator__user').filter(creator=profile)
+    return render(request, 'game/index.html',
+                  {'games': games,
                    'profile': profile})
